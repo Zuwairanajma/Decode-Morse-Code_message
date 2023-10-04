2d2ddb35b3d614659d894848946280e0abb61e51
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-10-04 15:27:19 +0100

    Update morse code

--- morse_message.rb
+++ morse_message.rb
@@ -1,31 +1,11 @@
 class MorseCode
   MORSE_CODE = {
     ' ' => ' ', # Add space without Morse code representation
-    '.-' => 'A',
-    '-...' => 'B',
-    '-.-.' => 'C',
-    '-..' => 'D',
-    '.' => 'E',
-    '..-.' => 'F',
-    '--.' => 'G',
-    '....' => 'H',
-    '..' => 'I',
-    '.---' => 'J',
-    '-.-' => 'K',
-    '.-..' => 'L',
-    '--' => 'M',
-    '-.' => 'N',
-    '---' => 'O',
-    '.--.' => 'P',
-    '--.-' => 'Q',
-    '.-.' => 'R',
-    '...' => 'S',
-    '-' => 'T',
-    '..-' => 'U',
-    '...-' => 'V',
-    '.--' => 'W',
-    '-..-' => 'X',
-    '-.--' => 'Y',
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E',
+    '..-.' => 'F', '--.' => 'G', '....' => 'H', '..' => 'I', '.---' => 'J',
+    '-.-' => 'K', '.-..' => 'L', '--' => 'M', '-.' => 'N', '---' => 'O',
+    '.--.' => 'P', '--.-' => 'Q', '.-.' => 'R', '...' => 'S', '-' => 'T',
+    '..-' => 'U', '...-' => 'V', '.--' => 'W', '-..-' => 'X', '-.--' => 'Y',
     '--..' => 'Z'
   }.freeze
 
@@ -33,36 +13,18 @@ class MorseCode
     MORSE_CODE[morse_char]
   end
 
-  def decode_char(morse_char)
-    MORSE_CODE[morse_char]
-  end
-
   def decode_word(morse_word)
-    MORSE_CODE[morse_word]
-    morse_letters = morse_word.split(/ +/)
-    word_decoded = ''
-
-    morse_letters.each do |morse_char|
-      char_decoded = decode_char(morse_char)
-      word_decoded += char_decoded if char_decoded
-    end
-
-    word_decoded
+    morse_word.split.map { |morse_char| decode_char(morse_char) }.compact.join('')
   end
 
   def decode(message)
-    morse_words = message.split('   ')
-    decoded_message = ''
-    morse_words.each do |morse_word|
-      decoded_word = decode_word(morse_word)
-      decoded_message += "#{decoded_word} " unless decoded_word.empty?
-    end
-    decoded_message.strip
+    message.split('   ').map { |morse_word| decode_word(morse_word) }.join(' ').strip
   end
 end
 
-message = '-- -.--   -. .- -- .'
 decoder = MorseCode.new
+
+message = '-- -.--   -. .- -- .'
 decoded_message = decoder.decode(message)
 puts "Input Morse Code: #{message}"
 puts "Decoded Message: #{decoded_message}"
